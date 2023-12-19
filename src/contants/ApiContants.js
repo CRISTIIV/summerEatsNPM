09789f12ba9a8b55201c7df4d6d5a1e72c1bebdf
Jeanmolinach <jean.molina.ch@gmail.com>
2023-12-19 03:00:42 -0300

    Intento de conexión a base de datos mongodb

---
+++ src/contants/ApiContants.js
@@ -0,0 +1,15 @@
+const config = require("../../package.json").projectConfig;
+const BACKEND_BASE_URL = config.backendApiBaseUrl;
+
+const BACKEND_API = {
+  BASE_API_URL: `${BACKEND_BASE_URL}/api`,
+  REGISTER: "/register",
+  LOGIN: "/login",
+  USER_EXIST: "/user-exist",
+  USER: "/user",
+  REFRESH_TOKEN: "/refresh-token",
+  CART: "/cart",
+  FOOD: "/food",
+};
+
+export default { BACKEND_API };
---
+++ src/services/AuthenticationService.js
@@ -0,0 +1,80 @@
+import axios from "axios";
+import { ApiContants } from "../contants";
+
+const AuthRequest = axios.create({
+  baseURL: ApiContants.BACKEND_API.BACKEND_API_URL,
+});
+
+const register = async (user) => {
+  if (!user?.username || !user?.email || !user?.password) {
+    return { status: false, message: "Porfavor, rellene todos los campos" };
+  }
+  try {
+    let requestBody = {
+      username: user?.username,
+      email: user?.email,
+      password: user?.password,
+    };
+    let registerResponse = await AuthRequest.post(
+      ApiContants.BACKEND_API.REGISTER,
+      requestBody
+    );
+    return registerResponse?.data;
+  } catch (error) {
+    console.log(error);
+    return { status: false, message: "¡Oh! Algo salio mal" };
+  }
+};
+
+const login = async (user) => {
+  if (!user?.username || !user?.password) {
+    return { status: false, message: "Porfavor, rellene todos los campos" };
+  }
+  try {
+    let requestBody = {
+      username: user?.username,
+      password: user?.password,
+    };
+    let loginResponse = await AuthRequest.post(
+      ApiContants.BACKEND_API.LOGIN,
+      requestBody
+    );
+    return loginResponse?.data;
+  } catch (error) {
+    console.log(error);
+    return { status: false, message: "¡Oh! Algo salio mal" };
+  }
+};
+
+const checkUserExist = async (type, value) => {
+  try {
+    let params = { [type]: value };
+    let userCheckResponse = await AuthRequest.get(
+      ApiContants.BACKEND_API.USER_EXIST,
+      { params }
+    );
+    return userCheckResponse?.data;
+  } catch (error) {
+    console.log(error);
+    return { status: false, message: "¡Oh! Algo salio mal" };
+  }
+};
+
+const refreshToken = async () => {
+  try {
+    let tokenResponse = await AuthRequest.post(
+      ApiContants.BACKEND_API.REFRESH_TOKEN,
+      { headers: authHeader(getToken()) }
+    );
+    if (tokenResponse?.status === 200) {
+      return { status: true, data: tokenResponse?.data };
+    } else {
+      return { status: false };
+    }
+  } catch (error) {
+    console.log(error);
+    return { status: false, message: "¡Oh! Algo salio mal" };
+  }
+};
+
+export default { register, login, checkUserExist, refreshToken };
